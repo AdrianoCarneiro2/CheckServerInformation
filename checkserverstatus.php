7ad0d8f336a7ed9300d5b03fa2629efc10e7de86
boxxerdc <adrianocarneiro378@gmail.com>
2021-02-07 19:30:51 +0000

    add tables information

---
+++ checkserverstatus.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Server status</title>
+    <meta content="text/html" charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        pre {
+            overflow-x: auto;
+            max-width: 60vw;
+        }
+
+        pre code {
+            word-wrap: normal;
+            white-space: pre;
+        }
+    </style>
+</head>
+<html>
+<div class="container">
+    <?php
+
+    ?>
+
+    <body>
+        <div class="card mb-2">
+            <h4 class="card-header text-center">
+                Server information
+            </h4>
+         
+        </div>
+
+        <div class="card mb-2">
+            <h4 class="card-header text-center">
+                Service Status
+            </h4>
+         
+        </div>
+    </body>
+</div>
+
+</html>
