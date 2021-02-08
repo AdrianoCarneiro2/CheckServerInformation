530874332d9e6d072eb53ee0345922d724d780f4
boxxerdc <adrianocarneiro378@gmail.com>
2021-02-08 22:43:31 +0000

    ram available and hard disk memory available

--- checkserverstatus.php
+++ checkserverstatus.php
@@ -22,7 +22,27 @@
 <html>
 <div class="container">
     <?php
+    //Get ram usage
+    $total_mem = preg_split('/ +/', @exec('grep MemTotal /proc/meminfo'));
+    $total_mem = $total_mem[1];
+    $free_mem = preg_split('/ +/', @exec('grep MemFree /proc/meminfo'));
+    $cache_mem = preg_split('/ +/', @exec('grep ^Cached /proc/meminfo'));
 
+    $percentvalue = $free_mem[1] / $total_mem;
+    $percentvalue = $percentvalue * 100;
+
+    $free_mem = $free_mem[1] + $cache_mem[1];
+
+/*
+* The disks array list all mountpoint you wan to check freespace
+* Display name and path to the moutpoint have to be provide, you can 
+*/
+ 
+    $diskfree = round(disk_free_space(".") / 1000000000);
+    $disktotal = round(disk_total_space(".") / 1000000000);
+    $diskused = round($disktotal - $diskfree);
+    $diskusage = round($diskused/$disktotal*100);
+    $disk_free_precent = 100 - round($diskfree*1.0 / $disktotal*100, 2);
     ?>
 
     <body>
@@ -30,14 +50,38 @@
             <h4 class="card-header text-center">
                 Server information
             </h4>
-         
+            <div>
+                Ram Memory
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped active" role="progressbar" 
+                aria-valuenow="<?php echo $free_mem ?>" 
+                aria-valuemin="0" 
+                aria-valuemax="<?php echo $total_mem ?>" 
+                style="width:<?php echo $percentvalue ."%" ?>">
+                <?php echo $percentvalue ."%" ?>
+                </div>
+            </div>
+            </div>
+
+            <div>
+                Hard Drive Space
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped active" role="progressbar" 
+                aria-valuenow="<?php echo $diskfree ?>" 
+                aria-valuemin="0" 
+                aria-valuemax="<?php echo $disktotal ?>" 
+                style="width:<?php echo $disk_free_precent ."%" ?>">
+                <?php echo $diskused ." Gb" ?>
+                </div>
+            </div>
+            </div>
+           
         </div>
 
         <div class="card mb-2">
             <h4 class="card-header text-center">
                 Service Status
             </h4>
-         
         </div>
     </body>
 </div>
